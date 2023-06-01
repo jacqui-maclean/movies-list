9ccb87c5939ff8b1f9accfae0f000415c775a44b
jacqui-maclean <inaripaddler@gmail.com>
2023-06-01 09:29:55 +0100

    DRY refactor

--- src/components/ToggleTheme.tsx
+++ src/components/ToggleTheme.tsx
@@ -4,25 +4,28 @@ interface Props {
   onToggle: () => void;
 }
 const ToggleTheme = ({ darkMode, onToggle }: Props) => {
-  return !darkMode ? (
+  return (
     <div onClick={onToggle} className="custom-control custom-switch mt-1">
-      <input
-        type="checkbox"
-        className="custom-control-input"
-        id="customSwitches"
-      />
-      <label className="custom-control-label">Switch to dark mode</label>
-    </div>
-  ) : (
-    <div onClick={onToggle} className="custom-control custom-switch mt-1">
-      <input
-        type="checkbox"
-        className="custom-control-input"
-        id="customSwitches"
-        checked
-      />
-      <label className="custom-control-label" style={{ color: "white" }}>
-        Switch to light mode
+      {darkMode ? (
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+          checked
+        />
+      ) : (
+        <input
+          type="checkbox"
+          className="custom-control-input"
+          id="customSwitches"
+        />
+      )}
+
+      <label
+        className="custom-control-label"
+        style={{ color: darkMode ? "white" : "black" }}
+      >
+        {darkMode ? "Switch to light mode" : "Switch to dark mode"}
       </label>
     </div>
   );
